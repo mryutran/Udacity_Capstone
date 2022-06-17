e1972b41f91cf6f0e4377a2e4bf51f048c20b81c
Phong Q Tran <Phong.QTran@coxautoinc.com>
2022-06-17 13:00:31 +0700

    test

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-docker build --platform linux/amd64 --tag=udacity_capstone .
+# docker build --platform linux/amd64 --tag=udacity_capstone .
+docker build --tag=udacity_capstone .
 # docker build --tag=udacity_capstone .
 docker image ls
